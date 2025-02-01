5476e59e25247f7bddd47b4749cd865b4d2fe4b6
Hari Krishnan <harikrishnan@Haris-MacBook-Air-2.local>
2025-02-01 09:39:16 +0530

    kk trading frontend 1.0.0

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,22 +1,23 @@
-// https://github.com/vitejs/vite/discussions/3448
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import jsconfigPaths from 'vite-jsconfig-paths';
 
 export default defineConfig(({ mode }) => {
-  // https://github.com/jpuri/react-draft-wysiwyg/issues/1317
   const env = loadEnv(mode, process.cwd(), '');
-  const API_URL = `${env.VITE_APP_BASE_NAME}`;
+  const API_URL = env.VITE_APP_BASE_NAME || '/'; // default to '/' if not set
   const PORT = 3000;
 
   return {
+    base: API_URL,
+    build: {
+      outDir: 'dist'
+    },
     server: {
-      // this ensures that the browser opens upon server start
       open: true,
-      // this sets a default port to 3000
       port: PORT,
       host: true
+      // historyApiFallback is handled by Vite automatically in dev mode.
     },
     preview: {
       open: true,
@@ -36,13 +37,6 @@ export default defineConfig(({ mode }) => {
           replacement: path.join(process.cwd(), 'src/$1')
         }
       ]
-    },
-    base: API_URL,
-    build: {
-      outDir: 'dist'
-    },
-    server: {
-      historyApiFallback: true
     },
     plugins: [react(), jsconfigPaths()]
   };
