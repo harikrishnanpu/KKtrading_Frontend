b4d783134c8d94c7008d5087fbd16d856728692e
Hari krishnan P U <harikrishnan9a@gmail.com>
2025-06-05 10:46:50 +0530

    socket

--- src/api/socket.js
+++ src/api/socket.js
@@ -2,6 +2,11 @@
 import { io } from 'socket.io-client';
 
 // Use your backend URL or local dev URL
-const socket = io('http://localhost:4000/');
+const socket = io('https://office.vrkkt.com/',{
+  transports: ['websocket'], // Optional: force WebSocket only
+});
+// const socket = io('http://localhost:4000/',{
+//   transports: ['websocket'], // Optional: force WebSocket only
+// });
 
 export default socket;
